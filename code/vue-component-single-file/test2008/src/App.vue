2bf1ed4abf68b158508bda323ed0985b1b99c368
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-02-26 19:06:12 +0800

    finish vue-cli vue component in single file

--- code/vue-component-single-file/test2008/src/App.vue
+++ code/vue-component-single-file/test2008/src/App.vue
@@ -16,14 +16,25 @@
       <div>slotttttttttttttttt</div>
     </navbar>
     <sidebar v-show="isShow"></sidebar>
+    <div v-hello>hellooooooooooooooo</div>
+    <img :src="imgUrl | imgFilter"/>
   </div>
 </template>
 
 <script>
 import navbar from './components/NavBar.vue'
 import sidebar from './components/SideBar.vue'
-// import Vue from 'vue'
+import Vue from 'vue'
 // Vue.component('NavBar', navbar)
+Vue.directive('hello', {
+  inserted (el, binding) {
+    console.log(el)
+    el.style.border = '1px solid blue'
+  }
+})
+Vue.filter('imgFilter', (path) => {
+  return path
+})
 export default {
   components: {
     navbar,
@@ -34,7 +45,8 @@ export default {
       myname: 'kerwin',
       mytext: '12321',
       datalist: [],
-      isShow: true
+      isShow: true,
+      imgUrl: 'https://s1.hdslb.com/bfs/static/laputa-home/client/assets/load-error.685235d2.png'
     }
   },
   computed: {
