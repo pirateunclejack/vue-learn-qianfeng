381aad22908d13a623c43be1e3b83755efd194b1
pirate <17961436+pirateunclejack@users.noreply.github.com>
2023-03-14 01:14:26 +0800

    vue3 difference excercise

--- code/vue3/test2008vue3/src/App.vue
+++ code/vue3/test2008vue3/src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    Hello vue3
+    <input type="text" v-model="mytext">
+    {{ mytext }}
+    <button>Button</button>
+  </div>
+  <Hello></Hello>
+  <div v-hello>v-hello</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import hello from '@/Hello.vue'
+export default {
+  data (){
+    return {
+      mytext: ''
+    }
+  },
+  components : {
+    hello
+  },
+  directives: {
+    hello: {
+      mounted(el){
+        console.log('mounted')
+        el.style.background = 'red'
+      }
     }
   }
 }
-</style>
+</script>
